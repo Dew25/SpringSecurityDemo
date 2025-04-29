7ee50857806b7c15f320d1d7f5e5a8052410f385
Dew25 <dew2@yandex.ru>
2025-04-29 10:57:44 +0300

    Подправили view

--- src/main/java/ee/ivkhkdev/SpringSecurityDemo/contollers/HomeController.java
+++ src/main/java/ee/ivkhkdev/SpringSecurityDemo/contollers/HomeController.java
@@ -8,17 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("info","Hello SKTV22");
+        model.addAttribute("info","Главная страница");
+        model.addAttribute("hello","Hello SKTV22");
         return "index";
     }
     @GetMapping("/user")
     public String user(Model model){
-        model.addAttribute("info","Hello User SKTV22");
+        model.addAttribute("info","Страница пользователя");
+        model.addAttribute("hello","Hello User SKTV22");
         return "user";
     }
     @GetMapping("/admin")
     public String admin(Model model){
-        model.addAttribute("info","Hello Admin SKTV22");
+        model.addAttribute("info","Страница администратора");
+        model.addAttribute("hello","Hello Admin SKTV22");
         return "admin";
     }
 }
